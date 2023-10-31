3c6a6f5bf63efda56b8012b57f47c019f6c5f2f1
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2023-10-31 17:40:43 +0100

    Update urls.py

--- server/djangobackend/urls.py
+++ server/djangobackend/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('' views.home, name='home'),
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
