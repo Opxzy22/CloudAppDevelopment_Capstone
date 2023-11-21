eabbe87789296f517cecfa073812eaea814815c3
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2023-11-21 19:21:14 +0100

    Update getDealerships.js

--- functions/getDealerships.js
+++ functions/getDealerships.js
@@ -1,10 +1,3 @@
-n() will be run when you invoke this action
-  *
-  *   * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-  *     *
-  *       * @return The output of this action, which must be a JSON object.
-  *         *
-  *           */
 const { CloudantV1 } = require("@ibm-cloud/cloudant");
 const { IamAuthenticator } = require("ibm-cloud-sdk-core");
 
