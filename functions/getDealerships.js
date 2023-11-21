e6520c7b66073718ed9d689c41d7d9bcd6924545
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2023-11-21 10:08:44 +0100

    Update getDealerships.js

--- functions/getDealerships.js
+++ functions/getDealerships.js
@@ -9,20 +9,36 @@ const { CloudantV1 } = require("@ibm-cloud/cloudant");
 const { IamAuthenticator } = require("ibm-cloud-sdk-core");
 
 function main(params) {
-	const cloudant = cloudant({
+	const cloudant = new CloudantV1({
 		url: params.COUCH_URL,
-		plugins: { iamauth { iamApiKey: params.IAM_API_KEY } }
+		authenticator: new IamAuthenticator ({ iamApiKey: params.IAM_API_KEY }),
 				});
 	const dealershipDB = cloudant.db.use('dealership');
+	if (params.state) {
+		return dealershipDB
+		  .find({
+		  .selector: { state: params.state },
+		  .field: ["id", "city", "state", "st", "address", "zip", "lat", "long"],
+		})
+		.then((result) => {
+			const dealerships = result.doc
+			return  { dealership };
+			)}
+	.catch((error) => {
+	  console.error('error getting dealership by state':, error)
+	  return { error: failed to get dealership by state }
+	       });
+	} else {
 		
-	return dealershipDB.list({include_docs: true})
+	return dealershipDB
+		.list({include_docs: true})
 		.then((body) => {
 			const dealerships = body.rows.map(row => row.doc);
 			return { dealerships };
 			    	    })
 	.catch((error) => {
-		console.error('Error getting dealerships':, error)
-		return { error: 'failed to get dealerships }; 
+	console.error('Error getting dealerships':, error);
+	return { error: 'failed to get dealerships }; 
 	});
 }
-
+}
