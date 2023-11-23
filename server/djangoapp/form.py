be4f6618151f39c113127e3e7de032c8df3333ef
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2023-11-23 01:24:15 +0100

    Create form.py

---
+++ server/djangoapp/form.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class signUpForm(forms.form):
+    username = forms.CharField(label='Username', max_length=100)
+    first_name = forms.CharField(label='First Name', max_length=100)
+    last_name = forms.CharField(label='Last Name', max_length=100)
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
