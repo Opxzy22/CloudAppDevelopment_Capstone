d318b1c880c42134abae16df9d159dba64275e8b
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2023-11-24 11:43:28 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,8 +5,19 @@ from requests.auth import HTTPBasicAuth
 
 
 # Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(url, **kwargs):
+  print(kwargs)
+  print("GET from: {} " .format(url))
+
+  try:
+    response = requests.get(url=COUCH_URL, params=kwargs, headers={'Content-Type': 'application/json'},
+#                                     auth=HTTPBasicAuth('apikey', IAM_API_KEY))
+  except:
+    print("Network exception error")
+    status_code = response.status_code
+    print("with status {} " .format(status_code))
+    json_data = json.load(response.text)
+    return json_data
 
 
 # Create a `post_request` to make HTTP POST requests
@@ -17,6 +28,27 @@ from requests.auth import HTTPBasicAuth
 # def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
+def get_dealers_from_cf(url, **kwargs):
+  results = []
+  # Call get_request with a URL parameter
+  json_data = get_request(url)
+
+  if json_data:
+    # Get the row list in JSON as dealers
+    dealers = json_data["rows"]
+
+     # For each dealer object
+    for dealer in dealers:
+      # Get its content in `doc` object
+      dealer_doc = dealer["doc"]
+      # Create a CarDealer object with values in `doc` object
+      dealer_obj = CarDealer(address=dealer_doc["address"], city=dealer_doc["city"], full_name=dealer_doc["full_name"],
+                                   id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
+                                   short_name=dealer_doc["short_name"],
+                                   st=dealer_doc["st"], zip=dealer_doc["zip"])
+            results.append(dealer_obj)
+  return results
+    
 
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
