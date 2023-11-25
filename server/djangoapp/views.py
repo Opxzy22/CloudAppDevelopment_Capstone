877a327fcc8441b7b8179bb62e30e70aae801be4
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2023-11-25 23:47:01 +0100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -93,11 +93,11 @@ def get_dealerships(request):
         url = "https://us-south.functions.appdomain.cloud/api/v1/web/a12ecb5a-f0a3-4a55-9a4a-b1b28c1fdc99/default/getDealerships"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
-        dealer_list[dealerships] = dealerships
+        dealer_list.extend(dealerships)
         # Concat all dealer's short name
         dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        return render(request, 'djangoapp/index.html', dealer_list)
+        return render(request, 'djangoapp/index.html', {'dealer_names': dealer_names})
 
 
 def get_dealershps_by_state(request, state):
